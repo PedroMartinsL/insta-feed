c195d5cae7d58ade0b5bb7ef627046e4ca0daa61
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-05-11 11:41:15 -0300

    Applying tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import userData from "./assets/users.json";
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
-import { FaRegComment } from 'react-icons/fa';
-import { FiSend } from 'react-icons/fi';
-import { BsBookmark, BsBookmarkFill, BsThreeDotsVertical } from 'react-icons/bs';
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
+import { FiSend } from "react-icons/fi";
+import {
+  BsBookmark,
+  BsBookmarkFill,
+  BsThreeDotsVertical,
+} from "react-icons/bs";
 
 function InstagramActions({
   liked,
   saved,
   onToggleLike,
-  onToggleSave
+  onToggleSave,
 }: {
   liked: boolean;
   saved: boolean;
@@ -18,17 +22,21 @@ function InstagramActions({
   onToggleSave: () => void;
 }) {
   return (
-    <div style={{ display: 'flex', gap: '12px', alignItems: 'center' }}>
-      <div onClick={onToggleLike} style={{ cursor: 'pointer' }}>
-        {liked ? <AiFillHeart color="red" size={35} /> : <AiOutlineHeart size={35} />}
+    <div className="flex items-center gap-3">
+      <div onClick={onToggleLike} className="cursor-pointer">
+        {liked ? (
+          <AiFillHeart color="red" size={35} />
+        ) : (
+          <AiOutlineHeart size={35} />
+        )}
       </div>
 
       <FaRegComment size={30} />
       <FiSend size={35} />
 
-      <div style={{ flex: 1 }} />
+      <div className="flex-1" />
 
-      <div onClick={onToggleSave} style={{ cursor: 'pointer' }}>
+      <div onClick={onToggleSave} className="cursor-pointer">
         {saved ? <BsBookmarkFill size={35} /> : <BsBookmark size={35} />}
       </div>
     </div>
@@ -48,12 +56,14 @@ type PostData = UserData & {
 
 function Header(props: UserData) {
   return (
-    <header>
-      <div className="person">
-      {props.photo && <img src={props.photo} alt="userPerfil" className="perfil"/>}
-      <h1>{props.name_id}</h1>
+    <header className="flex justify-between items-center pb-[20px]">
+      <div className="flex items-center gap-x-4">
+        {props.photo && (
+          <img src={props.photo} alt="userPerfil" className="cursor-pointer w-[60px] h-[60px] rounded-full" />
+        )}
+        <h1 className="font-bold">{props.name_id}</h1>
       </div>
-      <BsThreeDotsVertical size={30} style={{ cursor: 'pointer' }} />
+      <BsThreeDotsVertical size={30} className="cursor-pointer" />
     </header>
   );
 }
@@ -63,16 +73,18 @@ function Post(props: PostData) {
   const [save, setSave] = useState(false);
 
   return (
-    <section className="main-post">
-      <Header name_id={props.name_id} photo={props.photo}/>
-      <div className="response-interaction">
-        {props.imgPost && <img src={`${props.imgPost}`} alt="post" />}
-        <InstagramActions liked={like} saved={save} onToggleLike={() => setLike(!like)}
-          onToggleSave={() => setSave(!save)}/>
+    <section className="border border-gray-400/50 p-5 pb-15 px-7.5 py-5 shadow-lg w-[35%]">
+      <Header name_id={props.name_id} photo={props.photo} />
+      <div className="flex flex-col justify-center pb-[20px]">
+        {props.imgPost && <img src={`${props.imgPost}`} alt="post" className="pb-[20px] object-cover max-h-[30rem]"/>}
+        <InstagramActions
+          liked={like}
+          saved={save}
+          onToggleLike={() => setLike(!like)}
+          onToggleSave={() => setSave(!save)}
+        />
       </div>
-      <span>
-        {props.likes} Likes
-      </span>
+      <span className="text-gray-500">{props.likes} Likes</span>
       <p>
         {props.textPost ? (
           props.textPost
@@ -88,7 +100,7 @@ function Post(props: PostData) {
 
 function App() {
   return (
-    <div className="app">
+    <div className="flex flex-col items-center p-[20px] min-h-screen gap-[5px]">
       {userData.map((user, index) => (
         <Post
           key={index}
